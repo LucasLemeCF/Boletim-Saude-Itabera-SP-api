417cb5da421cf613fe0492473a8155e21511ae94
Lucas Leme Cardoso Ferreira <lucasleme09@gmail.com>
2024-07-27 18:35:29 -0300

    - Adicionado Swagger

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Boletim-Saude-Itabera-SP
- 
+
+API - Swagger: http://localhost:8080/api/swagger-ui/index.html
+Design - Figma: https://www.figma.com/design/Ena3r3kvJhocQlGNe5DT7i/Untitled?t=oyZD2n1bGHiyJxZm-0
+Arquitetura - Miro: https://miro.com/app/board/uXjVK5NG8zs=/
--- src/main/java/boletimdasaude/BoletimMedicoApplication.java
+++ src/main/java/boletimdasaude/BoletimMedicoApplication.java
@@ -1,9 +1,12 @@
 package boletimdasaude;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "API para gerenciamento de boletins de Itaberá"))
 public class BoletimMedicoApplication {
 
 	public static void main(String[] args) {
